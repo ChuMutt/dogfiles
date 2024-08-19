fcfe79c9e6d926507b5bfa2c37d157ce38a0babc
chumutt <chufilthymutt@gmail.com>
2024-08-19 14:25:43 -0500

    initialize

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+
+  description = "chunixos";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = {self, nixpkgs, home-manager, ... }:
+    let
+      lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+    nixosConfigurations = {
+      nixos = lib.nixosSystem { # change "nixos" to your username
+        inherit system;
+	modules = [ ./configuration.nix ];
+      };
+    };
+    homeConfigurations = {
+      chu = home-manager.lib.homeManagerConfiguration {
+	inherit pkgs;
+	modules = [ ./home.nix ];
+      };
+    };
+  };
+
+}
---
+++ home.nix
@@ -0,0 +1,144 @@
+{ config, pkgs, ... }:
+
+let
+  myShellAliases = {
+    supdate = "sudo nixos-rebuild switch --flake ~/.dotfiles";
+    hupdate = "home-manager switch --flake ~/.dotfiles";
+    update = "supdate && hupdate";
+
+    cp = "cp -iv";
+    mv = "mv -iv";
+    rm = "rm -vI";
+    bc = "bc -ql";
+    rsync = "rsync -vrPlu";
+    mkd = "mkdir -pv";
+    yt = "yt-dlp --embed-metadata -i";
+    yta = "yt -x -f bestaudio/best";
+    ytt = "yt --skip-download --write-thumbnail";
+    ffmpeg = "ffmpeg -hide_banner";
+    lsblk = "lsblk --output NAME,LABEL,TRAN,TYPE,SIZE,FSUSED,FSTYPE,MOUNTPOINT";
+
+    # Colorize commands when possible.
+    ls = "ls -hN --color=auto --group-directories-first";
+    grep = "grep --color=auto";
+    diff = "diff --color=auto";
+    ccat = "highlight --out-format=ansi";
+    ip = "ip -color=auto";
+  };
+in
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "chu";
+  home.homeDirectory = "/home/chu";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.05"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  # home.packages = [
+    # # Adds the 'hello' command to your environment. It prints a friendly
+    # # "Hello, world!" when run.
+    # pkgs.hello
+
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  # ];
+
+  home.packages = with pkgs; [
+    bc
+    rsync
+    ffmpeg
+    yt-dlp
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/chu/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    EDITOR = "neovim";
+    TERMINAL = "st";
+    TERMINAL_PROG = "st";
+    VISUAL = "emacs";
+    BROWSER = "librewolf";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # Zsh; which is also enabled system-wide in /etc/nixos/configuration.nix,
+  # as otherwise it wouldn't be able to source necessary files.
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
+    shellAliases = myShellAliases;
+
+    history = {
+      size = 10000;
+      path = "${config.xdg.dataHome}/zsh/history";
+    };
+
+  };
+
+  programs.bash = {
+    enable = true;
+    shellAliases = myShellAliases;
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "chumutt";
+    userEmail = "chufilthymutt@gmail.com";
+    extraConfig = {
+      init.defaultBranch = "main";
+    };
+  };
+}
