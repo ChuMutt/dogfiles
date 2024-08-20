3c7ca444749ab3484a15866bf740e67f9cbe250d
chumutt <chufilthymutt@gmail.com>
2024-08-19 20:49:12 -0500

    clarify name; still nix compat.

--- home.nix
+++ home.nix
@@ -1,6 +1,5 @@
-{ config, pkgs, ... }:
-{
-  imports = [ ./sh.nix ];
+{ config, pkgs, ... }: {
+  imports = [ ./shells.nix ];
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "chu";
