3a707245040c47ee7ef8a4ac7aa07a1736aec2dd
chumutt <chufilthymutt@gmail.com>
2024-08-19 21:38:04 -0500

    fix closure

--- flake.nix
+++ flake.nix
@@ -1,11 +1,12 @@
 {
-
   description = "chunixos";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager/master";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, home-manager, ... }:
