79ba245c2301859f0a3c038c877310c07e7dcf2f
chumutt <chufilthymutt@gmail.com>
2024-08-19 21:55:25 -0500

    doom emacs

--- home.nix
+++ home.nix
@@ -1,39 +1,9 @@
 { config, pkgs, ... }: {
   imports = [ ./shells.nix ]; # Do not rename to shell.nix: filename reserved.
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "chu";
   home.homeDirectory = "/home/chu";
-
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
+  # You should not change this value, even if you update Home Manager.
   home.stateVersion = "24.05"; # Please read the comment before changing.
-
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  # home.packages = [
-  # # Adds the 'hello' command to your environment. It prints a friendly
-  # # "Hello, world!" when run.
-  # pkgs.hello
-
-  # # It is sometimes useful to fine-tune packages, for example, by applying
-  # # overrides. You can do that directly here, just don't forget the
-  # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-  # # fonts?
-  # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-  # # You can also create simple shell scripts directly inside your
-  # # configuration. For example, this adds a command 'my-hello' to your
-  # # environment:
-  # (pkgs.writeShellScriptBin "my-hello" ''
-  #   echo "Hello, ${config.home.username}!"
-  # '')
-  # ];
 
   home.packages = with pkgs; [
     bc
@@ -45,47 +15,19 @@
     (ripgrep.override { withPCRE2 = true; })
     # emacs deps
     emacs-all-the-icons-fonts
-    nixfmt # :lang nix
+    nixfmt-rfc-style # :lang nix
 
     # fonts
     fontconfig
-    (nerdfonts.override { fonts = [ "FireCode" ]; }) # doom emacs default font
+    (nerdfonts.override { fonts = [ "FiraCode" ]; }) # doom emacs default font
 
   ];
+
   # Autoload fonts from packages installed via Home Manager
   fonts.fontconfig.enable = true;
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
+  home.file = { };
 
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
-
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/chu/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
     EDITOR = "neovim";
     TERMINAL = "st";
@@ -101,11 +43,8 @@
 
   home.sessionPath = [ "${config.xdg.configHome}/emacs/bin" ];
 
-  programs.emacs.enable = true;
-  programs.emacs.package = pkgs.emacs29;
-
   # Note! This must match $DOOMDIR
-  xdg.configFile."doom".source = ./doom;
+  # xdg.configFile."doom".source = ./doom;
 
   # Note! This must match $EMACSDIR
   xdg.configFile."emacs".source = builtins.fetchGit {
@@ -115,6 +54,11 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+  # Enable Emacs, choose package.
+  programs.emacs.enable = true;
+  programs.emacs.package = pkgs.emacs29;
+  # Run de-bootstrap.sh
 
   programs.git = {
     enable = true;
