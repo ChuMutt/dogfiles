561cd41f95fcad5ca084547d80093b4e523c0477
chumutt <chufilthymutt@gmail.com>
2024-08-22 11:16:59 -0500

    fix this as well

--- hm-bootstrap.sh
+++ hm-bootstrap.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env/ sh
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager &&
 nix-channel --update &&
 nix-shell '<home-manager>' -A install
