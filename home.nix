0a8136ca985918c34bf042f859c8adea243cdb42
chumutt <chufilthymutt@gmail.com>
2024-08-23 09:03:30 -0500

    attempt at xdg desktop portals

--- home.nix
+++ home.nix
@@ -100,4 +100,14 @@
   # Autoload fonts from packages installed via Home Manager
   fonts.fontconfig.enable = true;
 
+  xdg.portal= {
+    enable=true;
+    configPackages = with pkgs; [
+      xdg-desktop-portal-gtk
+    ];
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+    ];
+  };
+
 }
