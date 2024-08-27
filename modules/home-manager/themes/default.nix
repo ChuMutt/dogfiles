169f3a90c2d589c86cee96edfabca0524ee01626
chumutt <chufilthymutt@gmail.com>
2024-08-27 10:05:47 -0500

    clean

---
+++ modules/home-manager/themes/default.nix
@@ -0,0 +1,18 @@
+# (mkIf (cfg.wallpaper != null)
+# Set the wallpaper ourselves so we don't need .background-image and/or
+# .fehbg polluting $HOME
+(let
+  wCfg = config.services.xserver.desktopManager.wallpaper;
+  command = ''
+    if [ -e "$XDG_DATA_HOME/bg" ]; then
+      # ${pkgs.feh}/bin/feh --bg-${wCfg.mode} \
+        # ${optionalString wCfg.combineScreens "--no-xinerama"} \
+        # --no-fehbg \
+        xwallpaper --fill $XDG_DATA_HOME/bg #*
+    fi
+  '';
+in {
+  services.xserver.displayManager.sessionCommands = command;
+  hey.hooks.reload."10-wallpaper" = command;
+})
+# )
