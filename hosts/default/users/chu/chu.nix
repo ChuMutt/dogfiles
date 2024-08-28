e0fc8cc72efa4e7cde405579fa7053e27b313a6b
chumutt <chufilthymutt@gmail.com>
2024-08-28 00:58:56 -0500

    experiment / formatting

--- hosts/default/users/chu/chu.nix
+++ hosts/default/users/chu/chu.nix
@@ -1,9 +1,13 @@
 { config, lib, pkgs, ... }:
 
 {
-  users.users.chu = {
-    isNormalUser = true;
-    description = "chu";
-    extraGroups = [ "networkmanager" "wheel" ];
+  options = { };
+  config = {
+    users.users.chu = {
+      isNormalUser = true;
+      description = "chu";
+      extraGroups = [ "networkmanager" "wheel" ];
+      shell = pkgs.zsh;
+    };
   };
 }
