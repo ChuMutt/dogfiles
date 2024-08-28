61b7b52547913e69f13bef0b3666e9172850e1a3
chumutt <chufilthymutt@gmail.com>
2024-08-27 23:01:41 -0500

    formatting

--- modules/x11/startx.nix
+++ modules/x11/startx.nix
@@ -1,4 +1,8 @@
 { config, lib, pkgs, ... }: {
-  options = { startx.enable = lib.mkEnableOption "enables startx as display manager"; };
-  config = lib.mkIf config.startx.enable { services.xserver.displayManager.startx.enable = true; };
+  options = {
+    startx.enable = lib.mkEnableOption "enables startx as display manager";
+  };
+  config = lib.mkIf config.startx.enable {
+    services.xserver.displayManager.startx.enable = true;
+  };
 }
