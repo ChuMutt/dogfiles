b67b1ec309c46c44974194e48967886320aa1d7c
ChuMutt <chufilthymutt@gmail.com>
2024-09-01 01:54:08 -0500

    add custom install script

--- home.nix
+++ home.nix
@@ -39,10 +39,13 @@
     packages = with pkgs; [
 
       # custom scripts
+
       (writeShellScriptBin "chu-install-home-manager-unstable" ''
+        # home-manager is recommended for this setup
+        # this installs the standalone version (recommended)
         nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager &&
         nix-channel --update &&
-        nix-shell '<home-manager>' -A install
+        nix-shell '<home-manager>' -A install # then run home-manager switch --flake ~/.config/dogfiles/#chunix
       '')
 
       # cli
