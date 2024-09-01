d2e1521dfce7d892663ed55e2ad04d99d07903c7
ChuMutt <chufilthymutt@gmail.com>
2024-08-31 20:32:38 -0500

    work on chunix config & format config for vm

--- hosts/chunix/configuration.nix
+++ hosts/chunix/configuration.nix
@@ -2,66 +2,92 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    # Include user module
+    ./chu.nix
+    # Include user home configuration
+    inputs.home-manager.nixosModules.default
+    # Include system modules
+    ../../modules/nixos/default.nix
+  ];
+
+  chu.enable = true;
+  chu.userName = "chu";
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot = {
+    loader = {
+      # systemd-boot (UEFI)
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+    boot.initrd.luks.devices."luks-168c511d-53ec-40cf-bf4a-810d13d4e9d8".device =
+      "/dev/disk/by-uuid/168c511d-53ec-40cf-bf4a-810d13d4e9d8";
+  };
 
-  boot.initrd.luks.devices."luks-168c511d-53ec-40cf-bf4a-810d13d4e9d8".device = "/dev/disk/by-uuid/168c511d-53ec-40cf-bf4a-810d13d4e9d8";
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
+  networking = {
+    hostName = "chunix-vm"; # Define your hostname.
+    networkmanager.enable = true; # Enable networking
+    # Enables wireless support via wpa_supplicant.
+    wireless.enable = true; # Incompatible with NetworkManager.
+    # Configure network proxy if necessary
+    # proxy.default = "http://user:password@proxy:port/";
+    # proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  };
 
   # Set your time zone.
   time.timeZone = "America/Chicago";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "en_US.UTF-8";
+      LC_IDENTIFICATION = "en_US.UTF-8";
+      LC_MEASUREMENT = "en_US.UTF-8";
+      LC_MONETARY = "en_US.UTF-8";
+      LC_NAME = "en_US.UTF-8";
+      LC_NUMERIC = "en_US.UTF-8";
+      LC_PAPER = "en_US.UTF-8";
+      LC_TELEPHONE = "en_US.UTF-8";
+      LC_TIME = "en_US.UTF-8";
+    };
   };
 
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
+  services.xserver = {
+    enable = true;
 
-  # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
+    # Enable the KDE Plasma Desktop Environment.
+    # displayManager.sddm.enable = true;
+    # desktopManager.plasma6.enable = true;
 
-  # Enable the GNOME Desktop Environment.
+    # Enable the GNOME Desktop Environment.
+    # displayManager.gdm.enable = true;
+    # desktopManager.gnome.enable = true;
 
+    # Enable the suckless dynamic window manager.
+    windowManager.dwm.enable = true;
 
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
+    # Enable Emacs X Window Manager (EXWM)
+    # windowManager.exwm.enable = true;
+
+    # Configure keymap in X11
+    xkb.layout = "us";
+    xkb.variant = "";
   };
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
+
+  # Enable bluetooth.
+  hardware.bluetooth.enable = true;
 
   # Enable sound with pipewire.
   hardware.pulseaudio.enable = false;
@@ -89,7 +115,7 @@
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
       kdePackages.kate
-    #  thunderbird
+      #  thunderbird
 
       tldr
       neovim
@@ -104,10 +130,11 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
-  ];
+  environment.systemPackages = with pkgs;
+    [
+      #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+      #  wget
+    ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
