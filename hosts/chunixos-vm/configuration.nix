afced16feddedfefea1ff964e3cf21ec4df40219
ChuMutt <chufilthymutt@gmail.com>
2024-09-02 19:03:26 -0500

    consistent comment docs, mv firewall and unfree

--- hosts/chunixos-vm/configuration.nix
+++ hosts/chunixos-vm/configuration.nix
@@ -46,6 +46,11 @@
     # Configure network proxy if necessary
     # proxy.default = "http://user:password@proxy:port/";
     # proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+    # Open ports in the firewall.
+    # firewall.allowedTCPPorts = [ ... ];
+    # firewall.allowedUDPPorts = [ ... ];
+    # Or disable the firewall altogether.
+    # firewall.enable = false;
   };
 
   # Set your time zone.
@@ -179,10 +184,16 @@
     users = { "chu" = import ../../home.nix; };
   };
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
 
-  system.stateVersion = "24.11"; # Do not change.
+  # Allow unfree packages. Sorry, rms.
+  nixpkgs.config.allowUnfree = true;
 
   nix.settings.experimental-features = "nix-command flakes";
 
--- hosts/dogleash/configuration.nix
+++ hosts/dogleash/configuration.nix
@@ -46,6 +46,11 @@
     # Configure network proxy if necessary
     # proxy.default = "http://user:password@proxy:port/";
     # proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+    # Open ports in the firewall.
+    # firewall.allowedTCPPorts = [ ... ];
+    # firewall.allowedUDPPorts = [ ... ];
+    # Or disable the firewall altogether.
+    # firewall.enable = false;
   };
 
   # Set your time zone.
@@ -66,13 +71,31 @@
       LC_TIME = "en_US.UTF-8";
     };
   };
+
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
   services.xserver = {
-    # Enable the X11 windowing system.
     enable = true;
-    # Enable the 'dwm' window manager.
-    windowManager.dwm = { enable = true; };
+
+    # Enable the KDE Plasma Desktop Environment.
+    displayManager.sddm.enable = true;
+    desktopManager.plasma6.enable = true;
+
+    # Enable the GNOME Desktop Environment.
+    # displayManager.gdm.enable = true;
+    # desktopManager.gnome.enable = true;
+
+    # Enable the suckless dynamic window manager.
+    # windowManager.dwm.enable = true;
+
+    # Enable Emacs X Window Manager (EXWM)
+    # windowManager.exwm.enable = true;
+
     # Configure keymap in X11
-    xkb.layout = "us";
+    xkb = {
+      layout = "us";
+      variant = "";
+    };
   };
 
   # Enable CUPS to print documents.
@@ -80,9 +103,6 @@
 
   # Enable bluetooth.
   hardware.bluetooth.enable = true;
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
@@ -164,7 +184,16 @@
     users = { "chu" = import ../../home.nix; };
   };
 
-  system.stateVersion = "24.11"; # Do not change.
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+  # Allow unfree packages. Sorry, rms.
+  nixpkgs.config.allowUnfree = true;
 
   nix.settings.experimental-features = "nix-command flakes";
 
