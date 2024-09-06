a61942af8ae1b96cdbf68342c544c0796364e8e8
ChuMutt <chufilthymutt@gmail.com>
2024-09-06 12:27:54 -0500

    modifications for modularization plans

--- flake.nix
+++ flake.nix
@@ -1,38 +1,29 @@
 # flake.nix
-
 {
   description = "nixOS config flake";
-
   inputs = {
-
     nixpkgs.url = "nixpkgs/nixos-unstable";
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "nixpkgs/nixos-24.05";
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     # TODO: Agenix
     # agenix = {
     #   url = "github:ryantm/agenix";
     #   inputs.nixpkgs.follows = "nixpkgs";
     # };
-
     # TODO: Declarative partitions
     # disko = {
     #   url = "github:nix-community/disko";
     #   inputs.nixpkgs.follows = "nixpkgs";
     # };
-
     # blender-bin = {
     #   url = "github:edolstra/nix-warez?dir=blender";
     #   inputs.nixpkgs.follows = "nixpkgs-unstable";
     # };
-
     nixos-hardware.url = "github:nixos/nixos-hardware";
-
     # Emacs
     emacs-overlay = {
       url = "github:nix-community/emacs-overlay";
@@ -49,45 +40,43 @@
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
       lib = nixpkgs.lib;
-
-      systemSettings = {
-        system = "x86_64-linux";
-        pkgs = nixpkgs.legacyPackages.${system};
-        lib = nixpkgs.lib;
-        hostname = "chunix";
-        profile = "chunix";
-        timezone = "America/Chicago";
-        locale = "en_US.UTF-8";
-        bootMode = "uefi";
-        bootMountPath =
-          "/boot"; # mount path for efi boot partition; only used for uefi boot mode
-        grubDevice =
-          ""; # device identifier for grub; only used for legacy (bios) boot mode
-        gpuType = "amd";
-      };
-
-      userSettings = rec {
-        username = "chu";
-        name = "Chu the Pup";
-        email = "chufilthymutt@gmail.com";
-        dotfilesDir = "~/.config/dogfiles";
-        windowManager = "plasma";
-        displayManager = "lightdm";
-        browser = "firefox";
-        defaultRoamDir = "~/nextcloud/documents/org/roam";
-        term = "konsole";
-        font = "Noto Sans";
-        fontPkg = pkgs.noto-fonts;
-        editor = "nvim";
-        spawnEditor = if (editor == "emacsclient") then
-          "emacsclient -c -a 'emacs'"
-        else
-          (if ((editor == "vim") || (editor == "nvim")
-            || (editor == "nano")) then
-            "exec " + term + " -e " + editor
-          else
-            editor);
-      };
+      # systemSettings = {
+      #   system = "x86_64-linux";
+      #   pkgs = nixpkgs.legacyPackages.${system};
+      #   lib = nixpkgs.lib;
+      #   hostname = "chunix";
+      #   profile = "chunix";
+      #   timezone = "America/Chicago";
+      #   locale = "en_US.UTF-8";
+      #   bootMode = "uefi";
+      #   bootMountPath =
+      #     "/boot"; # mount path for efi boot partition; only used for uefi boot mode
+      #   grubDevice =
+      #     ""; # device identifier for grub; only used for legacy (bios) boot mode
+      #   gpuType = "amd";
+      # };
+      # userSettings = rec {
+      #   username = "chu";
+      #   name = "Chu the Pup";
+      #   email = "chufilthymutt@gmail.com";
+      #   dotfilesDir = "~/.config/dogfiles";
+      #   windowManager = "plasma";
+      #   displayManager = "lightdm";
+      #   browser = "firefox";
+      #   defaultRoamDir = "~/nextcloud/documents/org/roam";
+      #   term = "konsole";
+      #   font = "Noto Sans";
+      #   fontPkg = pkgs.noto-fonts;
+      #   editor = "nvim";
+      #   spawnEditor = if (editor == "emacsclient") then
+      #     "emacsclient -c -a 'emacs'"
+      #   else
+      #     (if ((editor == "vim") || (editor == "nvim")
+      #       || (editor == "nano")) then
+      #       "exec " + term + " -e " + editor
+      #     else
+      #       editor);
+      # };
     in {
       homeConfigurations = {
         chu = home-manager.lib.homeManagerConfiguration {
