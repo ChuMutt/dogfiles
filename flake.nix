597f73a9c8f874330fdb1a225908748e826785cc
chumutt <chufilthymutt@gmail.com>
2024-09-08 18:18:35 -0500

    spawn editor, nixpkgs-patched (not yet enabled)

--- flake.nix
+++ flake.nix
@@ -28,9 +28,11 @@
         profile = "work";
         timezone = "America/Chicago";
         locale = "en_US.UTF-8";
-        bootMode = "uefi"; #uefi or bios
-        bootMountPath = "/boot"; # mount path for efi boot partition; only used for uefi boot mode
-        grubDevice = ""; # device identifier for grub; only used for legacy (bios) boot mode
+        bootMode = "uefi"; # uefi or bios
+        bootMountPath =
+          "/boot"; # mount path for efi boot partition; only used for uefi boot mode
+        grubDevice =
+          ""; # device identifier for grub; only used for legacy (bios) boot mode
         gpuType = "vm"; # amd, intel, nvidia, or vm.
       };
       userSettings = rec {
@@ -46,7 +48,29 @@
         font = "Noto Sans";
         fontPkg = pkgs.noto-fonts;
         editor = "emacsclient";
+        # editor spawning translator
+        # generates a command that can be used to spawn editor inside a gui
+        # EDITOR and TERM session variables must be set in home.nix or other module
+        # I set the session variable SPAWNEDITOR to this in my home.nix for convenience
+        spawnEditor = if (editor == "emacsclient") then
+          "emacsclient -c -a 'emacs'"
+        else
+          (if ((editor == "vim") || (editor == "nvim")
+            || (editor == "nano")) then
+            "exec " + term + " -e " + editor
+          else
+            editor);
       };
+      # create patched nixpkgs
+      # nixpkgs-patched = (import inputs.nixpkgs {
+      #   system = systemSettings.system;
+      #   rocmSupport = (if systemSettings.gpu == "amd" then true else false);
+      # }).applyPatches {
+      #   name = "nixpkgs-patched";
+      #   src = inputs.nixpkgs;
+      # patches = [ ./patches/example.patch ];
+      # };
+
     in {
       homeConfigurations = {
         chu = home-manager.lib.homeManagerConfiguration {
