e5cf133fbc7331ee22c9122e72d135c27dcd1fea
chumutt <chufilthymutt@gmail.com>
2024-09-08 07:30:43 -0500

    clean this up

--- profiles/work/configuration.nix
+++ profiles/work/configuration.nix
@@ -41,8 +41,6 @@
   networking = {
     hostName = "chunixos-vm"; # Define your hostname.
     networkmanager.enable = true; # Enable networking
-    # Enables wireless support via wpa_supplicant.
-    # wireless.enable = true; # Incompatible with NetworkManager.
     # Configure network proxy if necessary
     # proxy.default = "http://user:password@proxy:port/";
     # proxy.noProxy = "127.0.0.1,localhost,internal.domain";
