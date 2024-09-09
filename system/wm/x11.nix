5420273b7a0bd4489f8785342429876da869c0e1
chumutt <chufilthymutt@gmail.com>
2024-09-08 20:40:17 -0500

    x11

---
+++ system/wm/x11.nix
@@ -0,0 +1,43 @@
+{ pkgs, ... }:
+
+{
+  imports = [ ./pipewire.nix ./dbus.nix ./gnome-keyring.nix ./fonts.nix ];
+
+  services = {
+    xserver = {
+      enable = true; # Enable the X11 windowing system.
+      xkb = { # Configure keymap in X11
+        layout = "us";
+        variant = "";
+        options = "caps:escape";
+      };
+    };
+    displayManager = {
+      sddm.enable = true; # KDE
+      # gdm.enable = true; # GNOME
+      # lightdm.enable = true; # Canonical
+      # startx.enable = true; # none
+    };
+
+    desktopManager = {
+      # Enable the KDE Plasma Desktop Environment.
+      plasma6.enable = true; # KDE
+      # gnome.enable = true; # GNOME
+    };
+
+    # windowManager = {
+    # dwm.enable = true; # suckless
+    # exwm.enable = true; # emacs
+    # };
+
+    # For virtual machine guests to enable a daemon allowing for clipboard (copy/paste) sharing.
+    spice-vdagentd.enable = true;
+
+    libinput = {
+      enable =
+        true; # Enable touchpad support (enabled default in most desktopManager).
+      touchpad.disableWhileTyping = true;
+    };
+
+  };
+}
