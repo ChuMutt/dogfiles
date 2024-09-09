b518622eb827ecd1f8854d3ba17c511065c005e3
chumutt <chufilthymutt@gmail.com>
2024-09-08 20:57:40 -0500

    cleanup

--- profiles/work/configuration.nix
+++ profiles/work/configuration.nix
@@ -121,24 +121,14 @@
 
   security = {
     sudo = { enable = true; };
-    # rtkit.enable = true;
   };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
   programs = {
-    mtr.enable = true;
-    gnupg.agent = {
-      enable = true;
-      enableSSHSupport = true;
-    };
-
     zsh.enable = true;
     nh.enable = true;
     firefox.enable = true;
     steam.enable = true;
     gamemode.enable = true;
-
   };
 
   # List services that you want to enable:
