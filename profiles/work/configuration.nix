7972a0ff3ff00e4a9434e3d8daff32dd0cb57202
chumutt <chufilthymutt@gmail.com>
2024-09-08 20:58:03 -0500

    clean up

--- profiles/work/configuration.nix
+++ profiles/work/configuration.nix
@@ -119,9 +119,7 @@
   startx.enable =
     true; # otherwise defaults to lightdm gtk greeter when you log in
 
-  security = {
-    sudo = { enable = true; };
-  };
+  security = { sudo = { enable = true; }; };
 
   programs = {
     zsh.enable = true;
@@ -144,23 +142,8 @@
   };
 
   # hardware = {
-    # Enable bluetooth.
-    # bluetooth.enable = true;
-    # Enable sound with pipewire.
-    # pulseaudio.enable = false;
-  # };
-
-  # services.pipewire = {
-  #   enable = true;
-  #   alsa.enable = true;
-  #   alsa.support32Bit = true;
-  #   pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    # jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+  # Enable sound with pipewire.
+  # pulseaudio.enable = false;
   # };
 
   home-manager = {
