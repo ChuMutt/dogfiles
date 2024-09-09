d04fdc9895b8c64ed705392c40717ead2ea8d2c9
chumutt <chufilthymutt@gmail.com>
2024-09-08 21:04:11 -0500

    add firewall module, ssh daemon module

---
+++ system/security/firewall.nix
@@ -0,0 +1,12 @@
+{ ... }:
+
+{
+  # Firewall
+  networking = {
+    # Open ports in the firewall.
+    # firewall.allowedTCPPorts = [ ... ];
+    # firewall.allowedUDPPorts = [ ... ];
+    # Or disable the firewall altogether.
+    # firewall.enable = false;
+  };
+}
