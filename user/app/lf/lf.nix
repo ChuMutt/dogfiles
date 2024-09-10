650f4672b485314e485aad971f900a3cab0d550b
chumutt <chufilthymutt@gmail.com>
2024-09-09 22:51:32 -0500

    lf

--- user/app/lf/lf.nix
+++ user/app/lf/lf.nix
@@ -1,10 +1,12 @@
 { config, pkgs, ... }: {
 
-  home.packages = with pkgs; [ lf ueberzugpp ];
-  home.file = {
-    ".config/lf/lfrc".source = ./lfrc;
-    ".config/lf/scope".source = ./scope;
-    ".config/lf/icons".source = ./icons;
-    ".config/lf/cleaner".source = ./cleaner;
+  home = {
+    packages = with pkgs; [ lf ueberzugpp ];
+    file = {
+      ".config/lf/lfrc".source = ./lfrc;
+      ".config/lf/scope".source = ./scope;
+      ".config/lf/icons".source = ./icons;
+      ".config/lf/cleaner".source = ./cleaner;
+    };
   };
 }
