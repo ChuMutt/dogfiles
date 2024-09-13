3ec993100609fe4b653dffffad01176db0bd2c3e
ChuMutt <chufilthymutt@gmail.com>
2024-09-13 12:18:38 -0500

    clean

--- user/app/terminal/konsole.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, lib, ... }:
-
-{
-  home.packages = with pkgs; [ kdePackages.konsole ];
-  programs.kdePackages.konsole.enable = true;
-  programs.kdePackages.konsole.settings = {
-    window.opacity = lib.mkForce 0.85;
-  };
-}
