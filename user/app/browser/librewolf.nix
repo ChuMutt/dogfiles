51bc9bae776279c339ccbdc81e0e4436dd43c9b7
chu <chufilthymutt@gmail.com>
2024-09-17 14:47:11 -0500

    fmt

--- user/app/browser/librewolf.nix
+++ user/app/browser/librewolf.nix
@@ -2,21 +2,25 @@
 
 {
   # Module installing librewolf as default browser
-  home.packages = if (userSettings.wmType == "wayland") then
-    [ pkgs.librewolf-wayland ]
-  else
-    [ pkgs.librewolf ];
+  home.packages =
+    if (userSettings.wmType == "wayland") then [ pkgs.librewolf-wayland ] else [ pkgs.librewolf ];
 
-  home.sessionVariables = if (userSettings.wmType == "wayland") then {
-    DEFAULT_BROWSER = "${pkgs.librewolf-wayland}/bin/librewolf";
-  } else {
-    DEFAULT_BROWSER = "${pkgs.librewolf}/bin/librewolf";
-  };
+  home.sessionVariables =
+    if (userSettings.wmType == "wayland") then
+      {
+        DEFAULT_BROWSER = "${pkgs.librewolf-wayland}/bin/librewolf";
+      }
+    else
+      {
+        DEFAULT_BROWSER = "${pkgs.librewolf}/bin/librewolf";
+      };
 
   home.file.".librewolf/librewolf.overrides.cfg".text =
     ''
-    defaultPref("font.name.serif.x-western","
-    '' + userSettings.font + ''
+      defaultPref("font.name.serif.x-western","
+    ''
+    + userSettings.font
+    + ''
       ");
           defaultPref("font.size.variable.x-western",20);
           defaultPref("browser.toolbars.bookmarks.visibility","always");
@@ -28,18 +32,20 @@
           defaultPref("gfx.webrender.software.opengl",false);
           defaultPref("webgl.disabled",true);
           pref("font.name.serif.x-western","
-          '' + userSettings.font + ''
-        ");
-            pref("font.size.variable.x-western",20);
-            pref("browser.toolbars.bookmarks.visibility","always");
-            pref("privacy.resisttFingerprinting.letterboxing", true);
-            pref("network.http.referer.XOriginPolicy",2);
-            pref("privacy.clearOnShutdown.history",true);
-            pref("privacy.clearOnShutdown.downloads",true);
-            pref("privacy.clearOnShutdown.cookies",true);
-            pref("gfx.webrender.software.opengl",false);
-            pref("webgl.disabled",true);
-      '';
+    ''
+    + userSettings.font
+    + ''
+      ");
+          pref("font.size.variable.x-western",20);
+          pref("browser.toolbars.bookmarks.visibility","always");
+          pref("privacy.resisttFingerprinting.letterboxing", true);
+          pref("network.http.referer.XOriginPolicy",2);
+          pref("privacy.clearOnShutdown.history",true);
+          pref("privacy.clearOnShutdown.downloads",true);
+          pref("privacy.clearOnShutdown.cookies",true);
+          pref("gfx.webrender.software.opengl",false);
+          pref("webgl.disabled",true);
+    '';
 
   xdg.mimeApps.defaultApplications = {
     "text/html" = "librewolf.desktop";
