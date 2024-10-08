d9304e0629a44e3177dcd57e4e00cd141df1e3b0
Chu the Pup <chufilthymutt@gmail.com>
2024-10-08 18:30:38 -0500

    emacs work

--- home.nix
+++ home.nix
@@ -116,44 +116,79 @@
         evil-collection
       ];
     extraConfig = ''
-            ;; Initialize vim keybindings
-            (evil-mode)
-            ;; Don't show a splash screen
-            (setq inhibit-startup-message t)
-            ;; Don't show a menu bar
-            (setq menu-bar-mode -1)
-            ;; Don't show a tool bar
-            (setq tool-bar-mode -1)
-            ;; Don't show a scroll bar
-            (setq scroll-bar-mode -1)
-            ;; Display line numbers in every buffer
-            (global-display-line-numbers-mode 1)
-            ;; Set standard indentation to 2 spaces
-            (setq standard-indent 2)
-            ;; Set frame fringe
-            (set-fringe-mode 10)
-            ;; Set up visual flashing bell
-            (setq visible-bell t)
-            ;; Make escape key (ESC) kill prompts
-            (global-set-key (kbd "<escape>" 'keyboard-escape-quit))
-            ;; Initialize package sources
-            (require 'package)
-            (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-      			       ("org" . "https://orgmode.org/elpa/")
-      			       ("elpa" . "https://elpa.gnu.org/packages/")))
-            (package-initialize)
+      ;; Initialize vim keybindings
+      (evil-mode)
 
-            (unless package-archive-contents
-       (package-refresh-contents))
+      ;; Don't show a splash screen
+      (setq inhibit-startup-message t)
 
+      ;; Don't show a menu bar
+      (setq menu-bar-mode -1)
+
+      ;; Don't show a tool bar
+      (setq tool-bar-mode -1)
+
+      ;; Don't show a scroll bar
+      (setq scroll-bar-mode -1)
+
+      ;; Display line numbers in every buffer
+      (global-display-line-numbers-mode 1)
+
+      ;; Set standard indentation to 2 spaces
+      (setq standard-indent 2)
+
+      ;; Set frame fringe
+      (set-fringe-mode 10)
+
+      ;; Set up visual flashing bell
+      (setq visible-bell t)
+
+      ;; Make escape key (ESC) kill prompts
+      (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+      ;; Initialize package sources
+      (require 'package)
+      (setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			       ("org" . "https://orgmode.org/elpa/")
+			       ("elpa" . "https://elpa.gnu.org/packages/")))
+      (package-initialize)
+      
+      (unless package-archive-contents
+        (package-refresh-contents))
+      
       ;; Initialize use-package on non-Linux platforms
       (unless (package-installed-p 'use-package)
-         (package-install 'use-package))
-
+        (package-install 'use-package))
+      
       (require 'use-package)
-      (setq use-package-always-ensure t)
 
+      (setq use-package-always-ensure t)
+      
+      ;; Start installing & configuring packages 
       (use-package command-log-mode)
+
+      (use-package doom-modeline
+        :ensure t
+        :init (doom-modeline-mode 1)
+        :custom ((doom-modeline-height 15)))
+      
+      (use-package all-the-icons)
+      
+      (use-package doom-themes
+        :init (load-theme 'doom-dracula t))
+      
+      (use-package rainbow-delimiters
+        :hook (prog-mode . rainbow-delimiters-mode))
+      
+      (use-package which-key
+        :init (which-key-mode)
+        :diminish which-key-mode
+        :config
+          (setq which-key-idle-delay 1))
+
+      (use-package ivy-rich
+        :init
+          (ivy-rich-mode 1))
 
     '';
   };
