48ab1bb57d1df55dc60d7975b820ba1d905c3f17
Chu the Pup <chufilthymutt@gmail.com>
2024-10-09 17:56:29 -0500

    emacs work

--- chumacs/init.el
+++ chumacs/init.el
@@ -85,5 +85,13 @@
                 eshell-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
+(setq
+  inhibit-startup-message t ; Don't show a splash screen
+  menu-bar-mode -1          ; Don't show a menu bar
+  tool-bar-mode -1          ; Don't show a tool bar
+  scroll-bar-mode -1        ; Don't show a scroll bar (doesn't do anything)
+  standard-indent 2         ; Set standard indentation to 2 spaces
+  visible-bell t)           ; Set up visual flashing bell
+
 ;; End Garbage Collection performance tweak:
 (setq gc-cons-threshold (* 2 1000 1000))
