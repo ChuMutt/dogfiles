5fa10d80c6e0ed857dceaf7bae7a2cd10ec5469c
ChuMutt <chufilthymutt@gmail.com>
2024-10-09 22:25:53 -0500

    doom themes and format

--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   # Home Manager needs a bit of information about you and the paths it should
@@ -126,50 +126,45 @@
 
     PS1=\"%B%{$fg[green]%}[%{$fg[magenta]%}%n%{$fg[green]%}@%{$fg[blue]%}%M %{$fg[magenta]%}%~%{$fg[green]%}]%{$reset_color%}$%b \"
 
-   # Basic auto/tab complete:
-   autoload -U compinit
-zstyle ':completion:*' menu select
-zmodload zsh/complist
-compinit
-_comp_options+=(globdots)		# Include hidden files.
+    # Basic auto/tab complete:
+    autoload -U compinit
+    zstyle ':completion:*' menu select
+    zmodload zsh/complist
+    compinit
+    _comp_options+=(globdots)		# Include hidden files.
 
-# vi mode
-bindkey -v
-export KEYTIMEOUT=1
+    # vi mode
+    bindkey -v
+    export KEYTIMEOUT=1
 
-# Use vim keys in tab complete menu:
-bindkey -M menuselect 'h' vi-backward-char
-bindkey -M menuselect 'k' vi-up-line-or-history
-bindkey -M menuselect 'l' vi-forward-char
-bindkey -M menuselect 'j' vi-down-line-or-history
-bindkey -v '^?' backward-delete-char
+    # Use vim keys in tab complete menu:
+    bindkey -M menuselect 'h' vi-backward-char
+    bindkey -M menuselect 'k' vi-up-line-or-history
+    bindkey -M menuselect 'l' vi-forward-char
+    bindkey -M menuselect 'j' vi-down-line-or-history
+    bindkey -v '^?' backward-delete-char
 
-# Use lf to switch directories and bind it to ctrl-o
-lfcd () {
-    tmp=\"$(mktemp -uq)\"
-    trap 'rm -f $tmp >/dev/null 2>&1 && trap - HUP INT QUIT TERM PWR EXIT' HUP INT QUIT TERM PWR EXIT
-    lf -last-dir-path=\"$tmp\" \"$@\"
-    if [ -f \"$tmp\" ]; then
-        dir=\"$(cat \"$tmp\")\"
-        [ -d \"$dir\" ] && [ \"$dir\" != \"$(pwd)\" ] && cd \"$dir\"
-    fi
-}
-bindkey -s '^o' '^ulfcd\n'
+    # Use lf to switch directories and bind it to ctrl-o
+    lfcd () {
+        tmp=\"$(mktemp -uq)\"
+        trap 'rm -f $tmp >/dev/null 2>&1 && trap - HUP INT QUIT TERM PWR EXIT' HUP INT QUIT TERM PWR EXIT
+        lf -last-dir-path=\"$tmp\" \"$@\"
+        if [ -f \"$tmp\" ]; then
+            dir=\"$(cat \"$tmp\")\"
+            [ -d \"$dir\" ] && [ \"$dir\" != \"$(pwd)\" ] && cd \"$dir\"
+        fi
+    }
+    bindkey -s '^o' '^ulfcd\n'
+    bindkey -s '^a' '^ubc -lq\n'
+    bindkey -s '^f' '^ucd \"$(dirname \"$(fzf)\")\"\n'
+    bindkey '^[[P' delete-char
 
-bindkey -s '^a' '^ubc -lq\n'
-
-bindkey -s '^f' '^ucd \"$(dirname \"$(fzf)\")\"\n'
-
-bindkey '^[[P' delete-char
-
-# Edit line in vim with ctrl-e:
-autoload edit-command-line; zle -N edit-command-line
-bindkey '^e' edit-command-line
-bindkey -M vicmd '^[[P' vi-delete-char
-bindkey -M vicmd '^e' edit-command-line
-bindkey -M visual '^[[P' vi-delete
- 
-
+    # Edit line in vim with ctrl-e:
+    autoload edit-command-line; zle -N edit-command-line
+    bindkey '^e' edit-command-line
+    bindkey -M vicmd '^[[P' vi-delete-char
+    bindkey -M vicmd '^e' edit-command-line
+    bindkey -M visual '^[[P' vi-delete
     "; # Extra commands that should be added to {file}.zshrc
   };
 
@@ -185,7 +180,7 @@ bindkey -M visual '^[[P' vi-delete
         command-log-mode
         doom-modeline
         all-the-icons
-        # doom-themes
+        doom-themes # optional
         which-key
         ivy-rich
         counsel
@@ -243,5 +238,4 @@ bindkey -M visual '^[[P' vi-delete
     # Whether to use the GnuPG key agent for SSH keys or not.
     enableSshSupport = true;
   };
-
 }
