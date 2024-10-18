ee4907ff14f1467e629b80a061d7167f09348bdd
ChuMutt <chufilthymutt@gmail.com>
2024-10-18 01:32:28 -0500

    wayland

--- system/wm/hyprland.nix
+++ system/wm/hyprland.nix
@@ -1,4 +1,36 @@
-{ ... }: {
-  programs.hyprland.enable = true;
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+{ inputs, pkgs, lib, ... }:
+let
+  pkgs-hyprland =
+    inputs.hyprland.inputs.nixpkgs.legacyPackages.${pkgs.stdenv.hostPlatform.system};
+in {
+  environment.sessionVariables.NIXOS_OZONE_WL = "1"; # req'd for standalone HM
+
+  # Import wayland config
+  imports = [ ./wayland.nix ./pipewire.nix ./dbus.nix ];
+
+  # Security
+  security = { pam.services.login.enableGnomeKeyring = true; };
+
+  services.gnome.gnome-keyring.enable = true;
+
+  programs = {
+    hyprland = {
+      enable = true;
+      package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+      xwayland = { enable = true; };
+      portalPackage = pkgs-hyprland.xdg-desktop-portal-hyprland;
+    };
+  };
+
+  services.xserver.excludePackages = [ pkgs.xterm ];
+
+  services.xserver = {
+    displayManager.sddm = {
+      enable = true;
+      wayland.enable = true;
+      enableHidpi = true;
+      theme = "chili";
+      package = pkgs.sddm;
+    };
+  };
 }
