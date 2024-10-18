6199d65851919c69ea42407825b91bae615c4458
ChuMutt <chufilthymutt@gmail.com>
2024-10-18 01:00:56 -0500

    hyprand work

--- user/wm/hyprland/hyprland.nix
+++ user/wm/hyprland/hyprland.nix
@@ -1,3 +1,12 @@
 { inputs, config, lib, pkgs, userSettings, systemSetting, ... }: {
-  wayland.windowManager.hyprland.systemd.variables = [ "--all" ];
+  wayland.windowManager.hyprland = {
+    enable = true;
+    systemd.variables = [ "--all" ];
+    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+    plugins = [
+      inputs.hyprland-plugins.packages.${pkgs.system}.hyprtrails
+      inputs.hyprland-plugins.packages.${pkgs.system}.hyprexpo
+      inputs.hyprgrass.packages.${pkgs.system}.default
+    ];
+  };
 }
