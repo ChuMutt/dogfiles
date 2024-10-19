cc4aabb4812dfde673661081aa1ddab608fa48ac
ChuMutt <chufilthymutt@gmail.com>
2024-10-19 18:39:09 -0500

    git, alacritty

---
+++ user/app/git/git.nix
@@ -0,0 +1,11 @@
+{ pkgs, userSettings, ... }: {
+  home.packages = [ pkgs.git ];
+  programs.git.enable = true;
+  programs.git.userName = userSettings.name;
+  programs.git.userEmail = userSettings.email;
+  programs.git.extraConfig = {
+    init.defaultBranch = "main";
+    safe.directory =
+      [ (userSettings.dotfilesDir) (userSettings.dotfilesDir + "/.git") ];
+  };
+}
