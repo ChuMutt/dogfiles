e21657e28666ea135570d604bfec7799c7cdec48
ChuMutt <chufilthymutt@gmail.com>
2024-10-19 19:09:10 -0500

    undo

--- profiles/work/configuration.nix
+++ profiles/work/configuration.nix
@@ -220,7 +220,7 @@
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  networking.firewall.enable = false;
+  # networking.firewall.enable = false;
 
   environment.shells = with pkgs; [ zsh ];
   users.defaultUserShell = pkgs.zsh;
