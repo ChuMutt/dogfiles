a03bccf408eedb5ba0287ab857bc270fcff558f7
ChuMutt <chufilthymutt@gmail.com>
2024-10-19 19:20:38 -0500

    fmt

--- user/wm/hyprland/hyprland.nix
+++ user/wm/hyprland/hyprland.nix
@@ -1,4 +1,5 @@
-{ inputs, config, lib, pkgs, userSettings, systemSettings, pkgs-nwg-dock-hyprland, ... }:
+{ inputs, config, lib, pkgs, userSettings, systemSettings
+, pkgs-nwg-dock-hyprland, ... }:
 let
   pkgs-hyprland =
     inputs.hyprland.inputs.nixpkgs.legacyPackages.${pkgs.stdenv.hostPlatform.system};
