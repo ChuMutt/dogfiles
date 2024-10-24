44df8b4161f76c6f934550405337ece7b1811926
Chu <chufilthymutt@gmail.com>
2024-10-23 22:07:43 -0500

    fix flake.lock file

--- system/hardware-configuration.nix
+++ system/hardware-configuration.nix
@@ -26,9 +26,7 @@
       options = [ "fmask=0022" "dmask=0022" ];
     };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/5cdc45d7-5084-4596-ae60-3ecfa74d5789"; }
-    ];
+  swapDevices = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
