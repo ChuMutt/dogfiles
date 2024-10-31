297207229b3e4a5396da7565b2bd4a25469b5f01
Chu <chufilthymutt@gmail.com>
2024-10-30 23:30:47 -0500

    update

--- flake.nix
+++ flake.nix
@@ -189,20 +189,21 @@
           };
         };
       };
-      # nixOnDroidConfigurations = {
-      #   inherit pkgs;
-      #   default = inputs.nix-on-droid.lib.nixOnDroidConfiguration {
-      #     modules = [ ./profiles/nix-on-droid/configuration.nix ];
-      #   };
-      #   extraSpecialArgs = {
-      #     # pass config variables from above
-      #     inherit pkgs-stable;
-      #     inherit pkgs-emacs;
-      #     inherit systemSettings;
-      #     inherit userSettings;
-      #     inherit inputs;
-      #   };
-      # };
+      nixOnDroidConfigurations = {
+        inherit pkgs;
+        default = inputs.nix-on-droid.lib.nixOnDroidConfiguration {
+          modules = [ ./profiles/nix-on-droid/configuration.nix ];
+        };
+        extraSpecialArgs = {
+          # pass config variables from above
+          inherit pkgs-stable;
+          inherit pkgs-emacs;
+          inherit systemSettings;
+          inherit userSettings;
+          inherit inputs;
+        };
+      };
+
       packages = forAllSystems (
         system:
         let
@@ -210,6 +211,7 @@
         in
         {
           default = self.packages.${system}.install;
+
           install = pkgs.writeShellApplication {
             name = "install";
             runtimeInputs = with pkgs; [ git ]; # I could make this fancier by adding other deps
@@ -220,6 +222,7 @@
 
       apps = forAllSystems (system: {
         default = self.apps.${system}.install;
+
         install = {
           type = "app";
           program = "${self.packages.${system}.install}/bin/install";
@@ -232,59 +235,49 @@
       url = "https://git.lix.systems/lix-project/nixos-module/archive/2.90.0.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     nixpkgs.url = "nixpkgs/nixos-unstable";
-
     nixpkgs-stable.url = "nixpkgs/nixos-24.05";
-
+    emacs-pin-nixpkgs.url = "nixpkgs/f72123158996b8d4449de481897d855bc47c7bf6";
     kdenlive-pin-nixpkgs.url = "nixpkgs/cfec6d9203a461d9d698d8a60ef003cac6d0da94";
-
     nwg-dock-hyprland-pin-nixpkgs.url = "nixpkgs/2098d845d76f8a21ae4fe12ed7c7df49098d3f15";
 
-    home-manager-unstable = {
-      url = "github:nix-community/home-manager/master";
+    home-manager-unstable.url = "github:nix-community/home-manager/master";
+    home-manager-unstable.inputs.nixpkgs.follows = "nixpkgs";
+
+    home-manager-stable.url = "github:nix-community/home-manager/release-24.05";
+    home-manager-stable.inputs.nixpkgs.follows = "nixpkgs-stable";
+
+    nix-on-droid = {
+      url = "github:nix-community/nix-on-droid/master";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager-unstable";
     };
-
-    home-manager-stable = {
-      url = "github:nix-community/home-manager/release-24.05";
-      inputs.nixpkgs.follows = "nixpkgs-stable";
-    };
-
-    # nix-on-droid = {
-    #   url = "github:nix-community/nix-on-droid/master";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    #   inputs.home-manager.follows = "home-manager-unstable";
-    # };
 
     hyprland = {
       url = "github:hyprwm/Hyprland/v0.44.1?submodules=true";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     hyprland-plugins = {
       type = "git";
       url = "https://code.hyprland.org/hyprwm/hyprland-plugins.git";
-      rev = "4d7f0b5d8b952f31f7d2e29af22ab0a55ca5c219"; #v0.44.1
+      rev = "4d7f0b5d8b952f31f7d2e29af22ab0a55ca5c219"; # v0.44.1
       inputs.hyprland.follows = "hyprland";
     };
-
     hyprlock = {
       type = "git";
       url = "https://code.hyprland.org/hyprwm/hyprlock.git";
       rev = "73b0fc26c0e2f6f82f9d9f5b02e660a958902763";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    hyprgrass.url = "github:horriblename/hyprgrass/427690aec574fec75f5b7b800ac4a0b4c8e4b1d5";
+    hyprgrass.inputs.hyprland.follows = "hyprland";
 
-    hyprgrass = {
-      url = "github:horriblename/hyprgrass/427690aec574fec75f5b7b800ac4a0b4c8e4b1d5";
-      inputs.hyprland.follows = "hyprland";
-    };
+    nix-doom-emacs.url = "github:nix-community/nix-doom-emacs";
+    nix-doom-emacs.inputs.nixpkgs.follows = "emacs-pin-nixpkgs";
 
-    nix-straight = {
-      url = "github:librephoenix/nix-straight.el/pgtk-patch";
-      flake = false;
-    };
+    nix-straight.url = "github:librephoenix/nix-straight.el/pgtk-patch";
+    nix-straight.flake = false;
+    nix-doom-emacs.inputs.nix-straight.follows = "nix-straight";
 
     nvchad = {
       url = "github:NvChad/starter";
@@ -295,57 +288,46 @@
       url = "github:emacs-eaf/emacs-application-framework";
       flake = false;
     };
-
     eaf-browser = {
       url = "github:emacs-eaf/eaf-browser";
       flake = false;
     };
-
     org-nursery = {
       url = "github:chrisbarrett/nursery";
       flake = false;
     };
-
     org-yaap = {
       url = "gitlab:tygrdev/org-yaap";
       flake = false;
     };
-
     org-side-tree = {
       url = "github:localauthor/org-side-tree";
       flake = false;
     };
-
     org-timeblock = {
       url = "github:ichernyshovvv/org-timeblock";
       flake = false;
     };
-
     org-krita = {
       url = "github:librephoenix/org-krita";
       flake = false;
     };
-
     org-xournalpp = {
       url = "gitlab:vherrmann/org-xournalpp";
       flake = false;
     };
-
     org-sliced-images = {
       url = "github:jcfk/org-sliced-images";
       flake = false;
     };
-
     magit-file-icons = {
       url = "github:librephoenix/magit-file-icons/abstract-icon-getters-compat";
       flake = false;
     };
-
     phscroll = {
       url = "github:misohena/phscroll";
       flake = false;
     };
-
     mini-frame = {
       url = "github:muffinmad/emacs-mini-frame";
       flake = false;
@@ -359,6 +341,5 @@
       url = "github:StevenBlack/hosts";
       flake = false;
     };
-
   };
 }
