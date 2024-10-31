cdd5aad76c6283727bec1272c5940502ee3a588f
Chu <chufilthymutt@gmail.com>
2024-10-30 23:05:53 -0500

    fix

--- sync.sh
+++ sync.sh
@@ -6,5 +6,5 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-$SCRIPT_DIR/sync-system-debug.sh
-$SCRIPT_DIR/sync-user-debug.sh
+$SCRIPT_DIR/sync-system.sh
+$SCRIPT_DIR/sync-user.sh
