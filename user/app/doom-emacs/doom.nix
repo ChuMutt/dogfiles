17d9d148c66c12606bc687bb60a21dd129f87f08
Chu <chufilthymutt@gmail.com>
2024-10-31 23:05:48 -0500

    splash

--- user/app/doom-emacs/doom.nix
+++ user/app/doom-emacs/doom.nix
@@ -4,4 +4,8 @@
     template = builtins.readFile ./themes/doom-stylix-theme.el.mustache;
     extension = ".el";
   };
+
+  home.file."${config.xdg.localShareHome}/random-splash-image-dir/chosen-splash-images/src/chu-the-pup-scene-queen-by-samariyuu.png" = {
+    source = ./../doom-emacs/res/chu-the-pup-scene-queen-by-samariyuu.png;
+  };
 }
